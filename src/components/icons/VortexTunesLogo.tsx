3fba61f98fed64089d58e57e56a3204e070ec209
Riza Pati <rizapatiid@gmail.com>
2025-06-01 13:53:59 +0000

    gunakan gambar saya

--- src/components/icons/VortexTunesLogo.tsx
+++ src/components/icons/VortexTunesLogo.tsx
@@ -1,42 +1,24 @@
 
+import Image from 'next/image';
 import type React from 'react';
+import { cn } from '@/lib/utils';
 
-interface VortexTunesLogoProps extends React.SVGProps<SVGSVGElement> {
-  // Props for className, width, height can be passed directly
+interface VortexTunesLogoProps {
+  className?: string;
 }
 
-const VortexTunesLogo: React.FC<VortexTunesLogoProps> = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 230 32" 
-    aria-label="VortexTunes Logo"
-    {...props}
-  >
-    {/* VT Symbol - Simplified representation */}
-    <g id="vt-symbol">
-      {/* Blue left V-stroke */}
-      <path d="M0 2 L6 2 L16 28 L10 28 Z" fill="#0059A9" />
-      {/* Yellow right T-stroke (also part of V) */}
-      <path d="M17 2 L23 2 L33 28 L27 28 Z" fill="#F9B200" />
-      {/* Top Blue Bar (to make V top flat and connect) */}
-      <path d="M0 0 L23 0 L21 2 L2 2 Z" fill="#0059A9" />
-       {/* Small Yellow top bar for T illusion */}
-      <path d="M21 0 L30 0 L28 2 L19 2 Z" fill="#F9B200" />
-    </g>
-
-    {/* Text: VORTEXTUNES */}
-    <text
-      x="40" 
-      y="23" 
-      fontFamily="Arial, Helvetica, sans-serif" 
-      fontSize="20"
-      fontWeight="bold"
-      letterSpacing="0.2"
-    >
-      <tspan fill="#0059A9">VORTEX</tspan>
-      <tspan fill="#F9B200">TUNES</tspan>
-    </text>
-  </svg>
-);
+const VortexTunesLogo: React.FC<VortexTunesLogoProps> = ({ className }) => {
+  return (
+    <div className={cn('relative', className)}>
+      <Image
+        src="/vortextunes-logo.png" // User needs to place this file in the public folder
+        alt="VortexTunes Logo"
+        fill
+        style={{ objectFit: 'contain' }} // 'contain' respects aspect ratio within bounds
+        priority // Assuming it's an important logo, possibly in LCP
+      />
+    </div>
+  );
+};
 
 export default VortexTunesLogo;
